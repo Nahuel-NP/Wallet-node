f5a6cb218cd65c416cf6fbeb8de9237dd95796af
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-15 22:34:17 -0300

    init server

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,13 @@
-const message = "Hello world";
-console.log(message);
+import { Router } from "express";
+import { Server } from "./presentation/server";
+
+function main() {
+  const server = new Server({
+    port: 3000,
+    routes: Router(),
+  });
+
+  server.start();
+}
+
+main();
