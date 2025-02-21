39089c054a55c09cb64e69d99ee0de51ab3ef1b6
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-21 12:29:24 -0300

    use auth routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,14 +1,11 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
+import { AuthRoutes } from "./auth/auth.route";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
-    router.get('/api',(req:Request,res:Response)=>{
-      res.json({
-        msg:'Hola mundo'
-      })
-    })
+    router.use("/api/auth", AuthRoutes.routes);
 
     return router;
   }
