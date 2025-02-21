eef1c1203c722fb21e0fa3931c76e64e9a16b7d9
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-21 12:09:15 -0300

    remove password from user entity

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -8,20 +8,19 @@ export class UserEntity {
     public readonly name:string,
     public readonly email:string,
     public readonly emailValidated:boolean,
-    public readonly password:string,
+
     public readonly isActive:boolean,
     public readonly img?:string,
   ){}
 
   static fromObject(object:{[key:string]:any}):UserEntity{
-    const {id,name,email,emailValidated,password,isActive,img} = object;
+    const {id,name,email,emailValidated,isActive,img} = object;
     if(!id) throw 'Id is required';
     if(!name) throw 'Name is required';
     if(!email) throw 'Email is required';
     if(!emailValidated) throw 'EmailValidated is required';
-    if(!password) throw 'Password is required';
 
-    return new UserEntity(id,name,email,emailValidated,password,isActive,img);
+    return new UserEntity(id,name,email,emailValidated,isActive,img);
   }
 
 
