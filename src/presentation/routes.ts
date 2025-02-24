7563e7838a9293921bc8de0ebf8f9db081c090be
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-24 15:55:48 -0300

    create transfer route

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { AuthRoutes } from "./auth/auth.route";
+import { TransferRoute } from "./transaction/transfer/transfer.route";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.use("/api/auth", AuthRoutes.routes);
+    router.use("/api/transfer", TransferRoute.routes);
 
     return router;
   }
---
+++ src/presentation/transaction/transfer/transfer.route.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { TransferController } from "./transfer.controller";
+import { TransferService } from "./transfer.service";
+import { AuthMiddlaware } from "../../middlewares/auth.midleware";
+
+export class TransferRoute {
+  static get routes(): Router {
+    const router = Router();
+    const transferService = new TransferService();
+    const transferController = new TransferController(transferService);
+    router.post("/", [AuthMiddlaware.validateJWT], transferController.transfer);
+    return router;
+  }
+}
