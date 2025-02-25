842b6881399a9cc0cdbbff16b00cbb290653a4d0
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-25 16:13:01 -0300

    create tx type

---
+++ src/config/types/tx.type.ts
@@ -0,0 +1,8 @@
+import type { Prisma, PrismaClient } from "@prisma/client";
+import type { DefaultArgs } from "@prisma/client/runtime/library";
+
+
+export type PrismaTx = Omit<
+  PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>,
+  "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends"
+>;
