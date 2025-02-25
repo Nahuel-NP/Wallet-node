4a139b0583c5efb42ba565c1cc5b04a4f6a2aeb9
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-25 16:12:53 -0300

    create transaction constants

---
+++ src/config/constants/transaction.constant.ts
@@ -0,0 +1,11 @@
+export const TRANSACTION_TYPE = {
+  TRANSFER: "transfer",
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
+};
+
+export const STATUS = {
+  PENDING:'pending',
+  COMPLETED:'completed',
+  REJECTED:'rejected'
+}
