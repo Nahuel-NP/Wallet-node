9b8c853669585e37540b92cc69687eb7fb524b1e
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-26 15:51:14 -0300

    implement make transfer and save log

--- src/presentation/transaction/transfer/transfer.service.ts
+++ src/presentation/transaction/transfer/transfer.service.ts
@@ -4,50 +4,153 @@ import { UserEntity } from "../../../domain/entities/user.entity";
 import { CustomError } from "../../../domain/errors/custom.error";
 import { PrismaTx } from "../../../config/types/tx.type";
 import { Wallet } from "@prisma/client";
+import {
+  STATUS,
+  TRANSACTION_TYPE,
+} from "../../../config/constants/transaction.constant";
 
 export class TransferService {
-  async makeTransfer(trasnferDto: TransferDto, user: UserEntity) {
+  async makeTransfer(
+    trasnferDto: TransferDto,
+    user: UserEntity
+  ): Promise<{ success: boolean; transactionId: string }> {
     const { cvuOrAlias, amount } = trasnferDto;
 
-    try {
-      await prisma.$transaction(async (tx) => {
-        const originWallet = await this.getWalletByUser(tx, user.id);
+    const transferTransaction = await prisma.$transaction(async (ctx) => {
+      // get origin wallet
+      const originWallet = await this.getWalletByUser(ctx, user.id);
 
-        if (Number(originWallet.balance) < amount) {
-          throw CustomError.badRequest("Insufficient balance");
-        }
+      if (Number(originWallet.balance) < amount) {
+        throw CustomError.badRequest("Insufficient balance");
+      }
 
-        const destinationWallet = await this.getWalletFromCvuOrAlias(
-          tx,
-          cvuOrAlias
-        );
+      // get destination wallet
+      const destinationWallet = await this.getWalletFromCvuOrAlias(
+        ctx,
+        cvuOrAlias
+      );
 
-        if (!destinationWallet) {
-          throw CustomError.notFound("Destination wallet not found");
-        }
-        if (destinationWallet.userId === user.id) {
-          throw CustomError.forbiden("You can't transfer to yourself");
-        }
-        if (destinationWallet.user.isActive === false) {
-          throw CustomError.forbiden("Destination user is not active");
-        }
+      if (!destinationWallet) {
+        throw CustomError.notFound("Destination wallet not found");
+      }
+      if (destinationWallet.userId === user.id) {
+        throw CustomError.forbiden("You can't transfer to yourself");
+      }
+      if (destinationWallet.user.isActive === false) {
+        throw CustomError.forbiden("Destination user is not active");
+      }
 
-        const [updatedFromWallet, updatedToWallet] = await Promise.all([
-          this.decreaseAmountWallet(tx, user.id, originWallet, amount),
-          this.increaseAmountWallet(tx, destinationWallet, amount),
-        ]);
-
-        // TODO:create operations
-        
+      // init transaction with pending state
+      const transaction = await this.initTransaction(ctx, {
+        transactionId: crypto.randomUUID(),
+        transactionType: "transfer",
+        amount: amount,
+        walletId: originWallet.id,
       });
-    } catch (error) {
-      console.log(error);
-    }
+
+      if (!transaction) {
+        throw CustomError.internalServer("Transaction not created");
+      }
+      try {
+        // update wallets
+        const [updatedFromWallet, updatedToWallet] = await Promise.all([
+          this.decreaseAmountWallet(ctx, user.id, originWallet, amount),
+          this.increaseAmountWallet(ctx, destinationWallet, amount),
+        ]);
+        if (!updatedFromWallet || !updatedToWallet) {
+          throw CustomError.internalServer("Wallet not updated");
+        }
+        // create operations
+        const debitOperation = await this.createOperation(ctx, {
+          transactionId: transaction.id,
+          amount: amount,
+          type: "debit",
+        });
+
+        const creditOperation = await this.createOperation(ctx, {
+          transactionId: transaction.id,
+          amount: amount,
+          type: "credit",
+        });
+
+        // update transaction
+        const updatedTransaction = await ctx.transaction.update({
+          where: {
+            id: transaction.id,
+          },
+          data: {
+            status: STATUS.COMPLETED,
+            operations: {
+              connect: [{ id: debitOperation.id }, { id: creditOperation.id }],
+            },
+          },
+        });
+
+        // create security log
+        await ctx.securityLog.create({
+          data: {
+            userId: user.id,
+            action: TRANSACTION_TYPE.TRANSFER,
+            ipAddress: "TODO",
+            userAgent: "TODO",
+          },
+        });
+
+        return { success: true, transactionId: updatedTransaction.id };
+      } catch (error) {
+        console.log(error);
+        await ctx.transaction.update({
+          where: { id: transaction.id },
+          data: { status: "failed" },
+        });
+
+        throw new Error(`Error en la transferencia: ${error}`);
+      }
+    });
+
+    return transferTransaction;
   }
 
-  private async createOperation() {
-    // create operation
+  private async initTransaction(
+    ctx: PrismaTx,
+    transactionOption: {
+      transactionId: string;
+      transactionType: string;
+      amount: number;
+      walletId: string;
+    }
+  ) {
+    const transacion = await ctx.transaction.create({
+      data: {
+        status: STATUS.PENDING,
+        transactionId: transactionOption.transactionId,
+        amount: transactionOption.amount,
+        transactionType: transactionOption.transactionType,
+        walletId: transactionOption.walletId,
+      },
+    });
+
+    return transacion;
   }
+
+  private async createOperation(
+    ctx: PrismaTx,
+    operationOptions: {
+      transactionId: string;
+      amount: number;
+      type: string;
+    }
+  ) {
+    const operation = await ctx.operation.create({
+      data: {
+        transactionId: operationOptions.transactionId,
+        amount: operationOptions.amount,
+        operationType: operationOptions.type,
+      },
+    });
+    return operation;
+  }
+
   private async increaseAmountWallet(
     tx: PrismaTx,
     destinationWallet: Wallet,
