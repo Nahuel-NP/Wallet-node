8f70b5b1151dd6245a20f49edcf0fcb1f72a09d4
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-26 20:54:57 -0300

    process transfer in background

--- src/presentation/transaction/transfer/transfer.service.ts
+++ src/presentation/transaction/transfer/transfer.service.ts
@@ -3,55 +3,74 @@ import { TransferDto } from "../../../domain/dtos/transactions/transfer.dto";
 import { UserEntity } from "../../../domain/entities/user.entity";
 import { CustomError } from "../../../domain/errors/custom.error";
 import { PrismaTx } from "../../../config/types/tx.type";
-import { Wallet } from "@prisma/client";
+import { Transaction, Wallet } from "@prisma/client";
 import {
   STATUS,
   TRANSACTION_TYPE,
 } from "../../../config/constants/transaction.constant";
 
 export class TransferService {
-  async makeTransfer(
-    trasnferDto: TransferDto,
-    user: UserEntity
-  ): Promise<{ success: boolean; transactionId: string }> {
+  async makeTransfer(trasnferDto: TransferDto, user: UserEntity) {
     const { cvuOrAlias, amount } = trasnferDto;
 
-    const transferTransaction = await prisma.$transaction(async (ctx) => {
-      // get origin wallet
-      const originWallet = await this.getWalletByUser(ctx, user.id);
+    // get origin wallet
+    const originWallet = await this.getWalletByUser(user.id);
 
-      if (Number(originWallet.balance) < amount) {
-        throw CustomError.badRequest("Insufficient balance");
-      }
+    if (Number(originWallet.balance) < amount) {
+      throw CustomError.badRequest("Insufficient balance");
+    }
 
-      // get destination wallet
-      const destinationWallet = await this.getWalletFromCvuOrAlias(
-        ctx,
-        cvuOrAlias
-      );
+    // get destination wallet
+    const destinationWallet = await this.getWalletFromCvuOrAlias(cvuOrAlias);
 
-      if (!destinationWallet) {
-        throw CustomError.notFound("Destination wallet not found");
-      }
-      if (destinationWallet.userId === user.id) {
-        throw CustomError.forbiden("You can't transfer to yourself");
-      }
-      if (destinationWallet.user.isActive === false) {
-        throw CustomError.forbiden("Destination user is not active");
-      }
+    if (!destinationWallet) {
+      throw CustomError.notFound("Destination wallet not found");
+    }
 
-      // init transaction with pending state
-      const transaction = await this.initTransaction(ctx, {
-        transactionId: crypto.randomUUID(),
-        transactionType: "transfer",
-        amount: amount,
-        walletId: originWallet.id,
-      });
+    if (destinationWallet.userId === user.id) {
+      throw CustomError.forbiden("You can't transfer to yourself");
+    }
 
-      if (!transaction) {
-        throw CustomError.internalServer("Transaction not created");
-      }
-      try {
+    if (destinationWallet.user.isActive === false) {
+      throw CustomError.forbiden("Destination user is not active");
+    }
+    // init transaction with pending state
+    const transaction = await this.initTransaction({
+      transactionId: crypto.randomUUID(),
+      transactionType: "transfer",
+      amount: amount,
+      walletId: originWallet.id,
+    });
+
+    if (!transaction) {
+      throw CustomError.internalServer("Transaction not created");
+    }
+
+    // process transfer in background
+    this.processTransfer({
+      amount,
+      originWallet,
+      destinationWallet,
+      transaction,
+      user,
+    });
+
+    // return transaction with pending state
+    return transaction;
+  }
+
+  private async processTransfer(options: {
+    amount: number;
+    originWallet: Wallet;
+    destinationWallet: Wallet;
+    transaction: Transaction;
+    user: UserEntity;
+  }) {
+    const { amount, originWallet, destinationWallet, transaction, user } =
+      options;
+
+    try {
+      await prisma.$transaction(async (ctx) => {
         // update wallets
         const [updatedFromWallet, updatedToWallet] = await Promise.all([
           this.decreaseAmountWallet(ctx, user.id, originWallet, amount),
@@ -97,30 +116,34 @@ export class TransferService {
         });
 
         return { success: true, transactionId: updatedTransaction.id };
-      } catch (error) {
-        console.log(error);
-        await ctx.transaction.update({
-          where: { id: transaction.id },
-          data: { status: "failed" },
-        });
+      });
+    } catch (error) {
+      console.log(error);
+      await prisma.transaction.update({
+        where: { id: transaction.id },
+        data: { status: "failed" },
+      });
 
-        throw new Error(`Error en la transferencia: ${error}`);
-      }
-    });
+      await prisma.securityLog.create({
+        data: {
+          userId: user.id,
+          action: TRANSACTION_TYPE.TRANSFER,
+          ipAddress: "TODO",
+          userAgent: "TODO",
+        },
+      });
 
-    return transferTransaction;
+      throw new Error(`Error en la transferencia: ${error}`);
+    }
   }
 
-  private async initTransaction(
-    ctx: PrismaTx,
-    transactionOption: {
-      transactionId: string;
-      transactionType: string;
-      amount: number;
-      walletId: string;
-    }
-  ) {
-    const transacion = await ctx.transaction.create({
+  private async initTransaction(transactionOption: {
+    transactionId: string;
+    transactionType: string;
+    amount: number;
+    walletId: string;
+  }) {
+    const transacion = await prisma.transaction.create({
       data: {
         status: STATUS.PENDING,
         transactionId: transactionOption.transactionId,
@@ -194,8 +217,8 @@ export class TransferService {
     return wallet;
   }
 
-  private async getWalletByUser(tx: PrismaTx, id: string) {
-    const originWallet = await tx.wallet.findFirst({
+  private async getWalletByUser(id: string) {
+    const originWallet = await prisma.wallet.findFirst({
       where: {
         userId: id,
       },
@@ -207,8 +230,8 @@ export class TransferService {
     return originWallet;
   }
 
-  private async getWalletFromCvuOrAlias(tx: PrismaTx, cvuOrAlias: string) {
-    const destinationWallet = await tx.wallet.findFirst({
+  private async getWalletFromCvuOrAlias(cvuOrAlias: string) {
+    const destinationWallet = await prisma.wallet.findFirst({
       where: {
         OR: [
           {
