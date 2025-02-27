32eda5db139d295f2d4071da513cc4eb0e7cdfdd
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-27 15:03:36 -0300

    call credit service and createCreditDto

--- src/presentation/credit/credit.controller.ts
+++ src/presentation/credit/credit.controller.ts
@@ -1,20 +1,31 @@
 import { Request, Response } from "express";
 import { CreditService } from "./credit.service";
 import { CustomError } from "../../domain/errors/custom.error";
+import { CreateCreditDto } from "../../domain/dtos/credit/createCredit.dto";
 
 export class CreditController {
   constructor(private readonly creditService: CreditService) {}
 
   private handleErrors = (error: unknown, res: Response) => {
     if (error instanceof CustomError) {
-      return res.send(error.message).json({ error: error.message })
+      return res.send(error.message).json({ error: error.message });
     }
     return res.status(500).json({ error: "Internal server error" });
   };
 
   makeCredit = (req: Request, res: Response) => {
+    const { user, amout } = req.body;
+
+    const [error, createCreditDto] = CreateCreditDto.create({
+      amout,
+    });
+
+    if (error) {
+      return res.status(400).json({ error });
+    }
+
     this.creditService
-      .getCredit()
+      .getCredit(user, createCreditDto!)
       .then((credit) => res.json(credit))
       .catch((error) => this.handleErrors(error, res));
   };
