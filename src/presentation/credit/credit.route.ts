cf90d0246208585fec63a566f45d7b697a09d723
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-27 14:31:22 -0300

    using controller and service in route

--- src/presentation/credit/credit.route.ts
+++ src/presentation/credit/credit.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import { CreditController } from "./credit.controller";
+import { CreditService } from "./credit.service";
 
 export class CreditRoutes {
   static get routes(): Router {
     const router = Router();
-
-    router.post("/", (req, res) => {
-      res.json("get credit");
-    });
+    const creditService = new CreditService();
+    const creditController = new CreditController(creditService);
+    router.post("/", creditController.makeCredit);
     return router;
   }
 }
