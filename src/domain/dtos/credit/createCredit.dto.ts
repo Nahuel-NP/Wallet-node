ed0a7aef2e3c54e91a74bd6781fc1c872ede9dbc
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-27 14:40:38 -0300

    create credit dto

---
+++ src/domain/dtos/credit/createCredit.dto.ts
@@ -0,0 +1,24 @@
+export class CreateCreditDto {
+  
+  constructor(private readonly amount: number) {}
+
+  static create(object: {
+    [key: string]: any;
+  }): [error?: string, createCreditDto?: CreateCreditDto] {
+    const { amount } = object;
+
+    if (!amount) {
+      return ["Amount is required"];
+    }
+
+    if (isNaN(amount)) {
+      return ["Amount must be a number"];
+    }
+
+    if (amount <= 0) {
+      return ["Amount must be greater than 0"];
+    }
+
+    return [undefined, new CreateCreditDto(amount)];
+  }
+}
