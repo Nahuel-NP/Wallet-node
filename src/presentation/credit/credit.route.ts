6d2d17a84fe9a0724693cb4bc01ef038c4d29eeb
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-27 15:04:10 -0300

    use authmiddleware to credit route

--- src/presentation/credit/credit.route.ts
+++ src/presentation/credit/credit.route.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { CreditController } from "./credit.controller";
 import { CreditService } from "./credit.service";
+import { AuthMiddlaware } from "../middlewares/auth.midleware";
 
 export class CreditRoutes {
   static get routes(): Router {
     const router = Router();
     const creditService = new CreditService();
     const creditController = new CreditController(creditService);
-    router.post("/", creditController.makeCredit);
+    router.post("/", [AuthMiddlaware.validateJWT], creditController.makeCredit);
     return router;
   }
 }
