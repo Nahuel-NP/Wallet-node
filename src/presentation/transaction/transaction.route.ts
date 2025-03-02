a8cf6c7f2fa82e09588c4f8f15c0f50c5cbb4325
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-03-01 23:11:39 -0300

    use authMiddleware

--- src/presentation/transaction/transaction.route.ts
+++ src/presentation/transaction/transaction.route.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
 import { TransactionController } from "./transaction.controller";
 import { TransactionService } from "./transaction.service";
+import { AuthMiddlaware } from "../middlewares/auth.midleware";
 
 export class TransactionRoutes {
   static get routes(): Router {
     const router = Router();
     const transactionService = new TransactionService();
     const transactionController = new TransactionController(transactionService);
-    router.get("/", transactionController.getTransactions);
+    router.get(
+      "/",
+      [AuthMiddlaware.validateJWT],
+      transactionController.getTransactions
+    );
     return router;
   }
 }
