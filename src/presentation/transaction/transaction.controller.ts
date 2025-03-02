0da2b03e81de28247cf88c837011cf89a55dd6db
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-03-01 23:12:02 -0300

    take data from body and query parameters

--- src/presentation/transaction/transaction.controller.ts
+++ src/presentation/transaction/transaction.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import { CustomError } from "../../domain/errors/custom.error";
 import { TransactionService } from "./transaction.service";
+import { PaginationDto } from "../../domain/dtos/shared/pagination.dto";
 
 export class TransactionController {
-  constructor(private transactionService:TransactionService) {}
+  constructor(private transactionService: TransactionService) {}
 
   private handleErrors(error: unknown, res: Response) {
     if (error instanceof CustomError) {
@@ -13,9 +14,20 @@ export class TransactionController {
   }
 
   getTransactions = (req: Request, res: Response) => {
+    const { page = 1, limit = 10 } = req.query;
+    const [paginationError, paginationDto] = PaginationDto.create(
+      +page,
+      +limit
+    );
+
+    if (paginationError)
+      return res.status(400).json({ error: paginationError });
+
+    const { user } = req.body;
+
     this.transactionService
-      .getTransactions()
-      .then((transactions) => res.json({transactions}))
+      .getTransactions(user, paginationDto!)
+      .then((transactions) => res.json({ transactions }))
       .catch((error) => this.handleErrors(error, res));
   };
 }
