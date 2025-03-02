a0a03a327563d5ef1d0867b9c53f7a63953b7230
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-03-01 22:49:00 -0300

    change transactions path

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { AuthRoutes } from "./auth/auth.route";
-import { TransferRoutes } from "./transaction/transfer/transfer.route";
-import { CreditRoutes } from "./credit/credit.route";
+import { TransferRoutes } from "./transactions/transfer/transfer.route";
+import { CreditRoutes } from "./transactions/credit/credit.route";
+
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.use("/api/auth", AuthRoutes.routes);
-    router.use("/api/transfer", TransferRoutes.routes);
-    router.use("/api/credit", CreditRoutes.routes);
+    router.use("/api/transactions/transfer", TransferRoutes.routes);
+    router.use("/api/transactions/credit", CreditRoutes.routes);
 
     return router;
   }
