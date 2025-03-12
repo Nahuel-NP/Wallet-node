c04ff317ba4f7bb7f939f7c1a534b5a84eb48f42
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-03-11 21:08:52 -0300

    add route for withdrawal

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -3,7 +3,7 @@ import { AuthRoutes } from "./auth/auth.route";
 import { TransferRoutes } from "./transactions/transfer/transfer.route";
 import { CreditRoutes } from "./transactions/credit/credit.route";
 import { TransactionRoutes } from "./transaction/transaction.route";
-
+import { WithdrawalRoutes } from "./transactions/withdrawal/withdrawal.route";
 
 export class AppRoutes {
   static get routes(): Router {
@@ -13,7 +13,8 @@ export class AppRoutes {
     router.use("/api/transaction", TransactionRoutes.routes);
     router.use("/api/transactions/transfer", TransferRoutes.routes);
     router.use("/api/transactions/credit", CreditRoutes.routes);
-    
+    router.use("/api/transactions/withdrawal", WithdrawalRoutes.routes);
+
     return router;
   }
 }
