46aaad2af8e31e6f347060806052fd3918f8eecf
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-03-12 14:04:08 -0300

    pass ipAddress and user agent to service

--- src/presentation/transactions/withdrawal/withdrawal.controller.ts
+++ src/presentation/transactions/withdrawal/withdrawal.controller.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from "express";
 import { WithdrawalService } from "./withdrawal.service";
+import { WithdrawalDto } from "../../../domain/dtos/withdrawal/withdrawal.dto";
 
 export class WithdrawalController {
   constructor(private withdrawalService: WithdrawalService) {}
 
   withdrawalMoney = (req: Request, res: Response) => {
+    const { user, amount } = req.body;
+    const [error, withdrawalDto] = WithdrawalDto.create({
+      amount,
+    });
+
+    if (error) {
+      return res.status(400).json({ error });
+    }
+
+    const ipAddress =
+      (req.headers["x-forwarded-for"] as string) || req.ip || "Unknown";
+
+    const userAgent = req.headers["user-agent"] || "Unknown";
+
     this.withdrawalService
-      .withdrawal()
+      .withdrawal(user, withdrawalDto!, ipAddress, userAgent)
       .then((withdrawal) => res.json(withdrawal));
   };
 }
